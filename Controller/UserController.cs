54a9ff1c6d1c12f044e010f8d09f9c6139c6a924
19522144 <19522144@gm.uit.edu.vn>
2021-07-06 20:06:25 +0700

    Update database

---
+++ Controller/UserController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Management.Controller
+{
+    class UserController
+    {
+        KHACHSANEntities entities = new KHACHSANEntities();
+
+        public dynamic getAll()
+        {
+            var data = from c in entities.NGUOIDUNGs
+                       select new
+                       {
+                           ID = c.MANGUOIDUNG,
+                           Name = c.TENNGUOIDUNG,
+                           Rights = c.QUYENTRUYCAP,
+                           Avatar = c.ANHDAIDIEN
+                       };
+                       
+            return data.ToList();
+        }
+
+        public void UpdateUser(NGUOIDUNG nguoidung)
+        {
+            NGUOIDUNG p = entities.NGUOIDUNGs.Find(nguoidung.MANGUOIDUNG);
+            p.TENNGUOIDUNG = nguoidung.TENNGUOIDUNG;
+            p.TENDANGNHAP = nguoidung.TENDANGNHAP;
+            p.MATKHAU = nguoidung.MATKHAU;
+            p.QUYENTRUYCAP = nguoidung.QUYENTRUYCAP;
+
+            entities.SaveChanges();
+        }
+
+        public void updateRights(NGUOIDUNG nguoiDung)
+        {
+            NGUOIDUNG p = entities.NGUOIDUNGs.Find(nguoiDung.MANGUOIDUNG);
+            p.QUYENTRUYCAP = nguoiDung.QUYENTRUYCAP;
+
+            entities.SaveChanges();
+        }
+
+        public void deleteUser(int ID)
+        {
+            NGUOIDUNG nd = entities.NGUOIDUNGs.Find(ID);
+            if (nd != null)
+            {
+                entities.NGUOIDUNGs.Remove(nd);
+                entities.SaveChanges();
+            }
+        }
+
+        public void addUser(NGUOIDUNG nd)
+        {
+            entities.NGUOIDUNGs.Add(nd);
+            entities.SaveChanges();
+        }
+    }
+}
